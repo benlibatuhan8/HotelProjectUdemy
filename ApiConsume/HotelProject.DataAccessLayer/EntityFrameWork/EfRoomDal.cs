2bdc6a34f8688f15c67314b749407b0dcdae0964
Batuhan Benli <benlibatuhan6@hotmail.com>
2024-02-04 23:00:44 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFrameWork/EfRoomDal.cs
@@ -0,0 +1,22 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.EntityFrameWork
+{
+    public class EfRoomDal : GenericRepository<Room>,IRoomDal
+    {
+
+        public EfRoomDal(Context context):base(context)
+        {
+            
+        }
+    }
+}
---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFrameWork/EfServiceDal.cs
@@ -0,0 +1,21 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.EntityFrameWork
+{
+    public class EfServiceDal:GenericRepository<Service>,IServicesDal
+    {
+        public EfServiceDal(Context context):base(context)
+        {
+            
+        }
+    }
+}
